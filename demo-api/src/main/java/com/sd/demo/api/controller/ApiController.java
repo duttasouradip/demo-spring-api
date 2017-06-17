dc2fc193d94b0e548968bfab6d7e6631b8fc583a
souradip <souradip@souradip>
2017-06-17 21:14:26 +0530

    initial commit

---
+++ demo-api/src/main/java/com/sd/demo/api/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.sd.demo.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello World";
+	}
+	
+	
+}
